bd72412bd5516ffd1582710c0fdb24049a7da5e2
Tomasz Cielecki <tomasz@ostebaronen.dk>
2019-10-15 20:54:46 +0200

    Document enumerations

--- AndHUD/MaskType.cs
+++ AndHUD/MaskType.cs
@@ -1,9 +1,23 @@
 ﻿namespace AndroidHUD
 {
+    /// <summary>
+    /// Mask type to determine how to dim the background.
+    /// </summary>
     public enum MaskType
     {
+        /// <summary>
+        /// No mask type
+        /// </summary>
         None = 1,
+
+        /// <summary>
+        /// Show on top of clear background
+        /// </summary>
         Clear = 2,
+
+        /// <summary>
+        /// Show on top of black dimmed background
+        /// </summary>
         Black = 3
     }
 }
--- AndHUD/XHUD.MaskType.cs
+++ AndHUD/XHUD.MaskType.cs
@@ -1,8 +1,18 @@
 ﻿namespace XHUD
 {
+    /// <summary>
+    /// MaskType which maps to <see cref="AndroidHUD.MaskType"/>.
+    /// </summary>
     public enum MaskType
     {
+        /// <summary>
+        /// Show on top of clear background
+        /// </summary>
         Clear = 2,
+
+        /// <summary>
+        /// Show on top of black dimmed background
+        /// </summary>
         Black = 3,
     }
 }
